e3e1e3f335b97ad7f8b6cb333e5e42a7d25e0cac
mateo2208 <mateoandres2208@gmail.com>
2025-06-15 18:00:15 -0400

    ya

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, s as start } from './chunks/entrypoint_7oH1D3SA.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Ce-cGtO5.mjs';
-export { renderers } from '../renderers.mjs';
